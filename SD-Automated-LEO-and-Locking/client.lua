3a6ea212fd402a17b54aab7885b6d4052a0e1867
Novy <thornzz2019@gmail.com>
2022-06-10 19:56:15 +0100

    Version 1.0.0

---
+++ SD-Automated-LEO-and-Locking/client.lua
@@ -0,0 +1,33 @@
+--[[
+Copyright (c) 2022, Sandy Development
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+--]]
+
+Citizen.CreateThread(function()
+	while true do
+    playerped = GetPlayerPed(-1)
+    person_owns_vehicle = IsPedTryingToEnterALockedVehicle(playerPed)
+    player_is_emerg_driver = false
+    player_is_own_vehicle = false
+    veh = GetVehiclePedIsUsing(playerped)
+    lastveh = GetPlayersLastVehicle()
+    if IsVehicleSirenOn(veh) and IsPedInAnyVehicle(playerped, true) and IsControlPressed(0, 49) then
+        player_is_emerg_driver = true
+        SetVehicleSiren(veh, false)
+        Wait(1)
+        --SetPlayersLastVehicle(veh)
+        SetVehicleSiren(veh, true)
+        player_is_emerg_driver = false
+        player_is_own_vehicle = true
+    end
+    if player_is_own_vehicle then
+        SetVehicleDoorsLockedForAllPlayers(veh, true)
+        player_is_own_vehicle = false
+        -- More to be added here, newer version soon.
+		end
+	Citizen.Wait(0)
+	end
+end)
---
+++ SD-Automated-LEO-and-Locking/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sandy Development'
+version '1.0.0'
+
+client_script 'client.lua'
+--server_script 'server.lua'
+
+
+--[[
+    Script developed by Sandy Development. https://discord.gg/aWtauUMK7j
+--]]
