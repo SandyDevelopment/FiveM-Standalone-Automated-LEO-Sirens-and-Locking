fdb7c1ba25d73b8d7ed85bdfe3856dfff9056cd7
Novy <thornzz2019@gmail.com>
2022-06-10 22:08:38 +0100

    Changed fxmanifest.lua

--- SD-Automated-LEO-and-Locking/fxmanifest.lua
+++ SD-Automated-LEO-and-Locking/fxmanifest.lua
@@ -5,9 +5,8 @@ author 'Sandy Development'
 version '1.0.0'
 
 client_script 'client.lua'
---server_script 'server.lua'
 
 
 --[[
-    Script developed by Sandy Development. https://discord.gg/aWtauUMK7j
+    Script developed by Sandy Development. https://discord.gg/aWtauUMK7j (Discord)
 --]]
