6fec716c7141571528441c6d945a502a6a1c3589
Novy <55965858+thornzz2019@users.noreply.github.com>
2022-06-13 22:43:51 +0100

    Update client.lua

--- SD-Automated-LEO-and-Locking/client.lua
+++ SD-Automated-LEO-and-Locking/client.lua
@@ -8,23 +8,23 @@ LICENSE file in the root directory of this source tree.
 
 CreateThread(function()
 	while true do
-    playerped = PlayerPedId()
-    person_owns_vehicle = IsPedTryingToEnterALockedVehicle(playerPed)
-    player_is_emerg_driver = false
-    player_is_own_vehicle = false
-    veh = GetVehiclePedIsUsing(playerped)
-    if IsVehicleSirenOn(veh) and IsPedInAnyVehicle(playerped, true) and IsControlPressed(0, 49) then
-        player_is_emerg_driver = true
-        SetVehicleSiren(veh, false)
-        Wait(1)
-        --SetPlayersLastVehicle(veh)
-        SetVehicleSiren(veh, true)
-        player_is_emerg_driver = false
-        player_is_own_vehicle = true
-    end
-    if player_is_own_vehicle then
-        SetVehicleDoorsLockedForAllPlayers(veh, true)
-        player_is_own_vehicle = false
+	    playerped = PlayerPedId() -- Get player ped
+	    person_owns_vehicle = IsPedTryingToEnterALockedVehicle(playerPed) -- This is going to be used for the next version. 	
+	    player_is_emerg_driver = false	
+	    player_is_own_vehicle = false
+	    veh = GetVehiclePedIsUsing(playerped) -- Get vehicle ped is in
+			
+	    if IsVehicleSirenOn(veh) and IsPedInAnyVehicle(playerped, true) and IsControlPressed(0, 49) then
+		player_is_emerg_driver = true
+		SetVehicleSiren(veh, false)
+		Wait(1)
+		SetVehicleSiren(veh, true)
+		player_is_emerg_driver = false
+		player_is_own_vehicle = true
+    		end
+	    if player_is_own_vehicle then
+		SetVehicleDoorsLockedForAllPlayers(veh, true)
+		player_is_own_vehicle = false
         -- More to be added here, newer version soon.
 		end
 	Wait(0)
